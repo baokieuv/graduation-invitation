4069f96ba970935d5772ca12e1ad3437402c8984
baokieuv <154108649+baokieuv@users.noreply.github.com>
2026-09-23 07:40:27 +0700

    fix: cors

--- server/src/main/java/com/graduation/letter/config/SecurityConfig.java
+++ server/src/main/java/com/graduation/letter/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.graduation.letter.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,6 +29,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 // 1. Disable CSRF as we are building a stateless API
                 .csrf(csrf -> csrf.disable())
 
